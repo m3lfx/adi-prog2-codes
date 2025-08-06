9c19c3af67b4287df0b479d6c90c30a2a45956a3
rommel dalisay <rommelmdalisay@gmail.com>
2025-08-06 16:19:07 +0800

    write names

---
+++ write_names.py
@@ -0,0 +1,21 @@
+def main():
+    # Get three names.
+    print('Enter the names of three friends.')
+    name1 = input('Friend #1: ')
+    name2 = input('Friend #2: ')
+    name3 = input('Friend #3: ')
+
+    # Open a file named friends.txt.
+    myfile = open('friends.txt', 'w')
+
+    # Write the names to the file.
+    myfile.write(name1 + '\n')
+    myfile.write(name2 + '\n')
+    myfile.write(f"{name3}\n")
+
+    # Close the file.
+    myfile.close()
+    print('The names were written to friends.txt.')
+
+# Call the main function.
+main()
