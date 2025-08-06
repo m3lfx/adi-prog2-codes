f7b06727ca8eb7fed77c64277317000cf6c1a39d
rommel dalisay <rommelmdalisay@gmail.com>
2025-08-06 14:19:28 +0800

    rainfall

--- rain_fall.py
+++ rain_fall.py
@@ -3,9 +3,17 @@ def main():
     compute_rainfall(years)
 
 def compute_rainfall(years):
+    total = 0
     for i in range(1, years+1):
-        # rain_inches = float(input("enter rainfall measurement for month  " + str(i)))
-        rain_inches = float(input(f"enter rainfall measurement for month  {i}" ))
+        print(f"year number {i}")
+        for month in range(1, 13):
+            # rain_inches = float(input("enter rainfall measurement for month  " + str(i)))
+            rain_inches = float(input(f"enter rainfall measurement for month  {month}: " ))
+            total += rain_inches
+        
+        # total_rainfall += total
+    print(f"total rainfall in inches is {total}")
+    print(f"average rainfall in {years} years is {total/years}")
 
 
 main()
