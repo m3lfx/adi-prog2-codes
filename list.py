aa24c0962a18b261d5c3d7e6aa72f7bee3e08f07
rommel dalisay <rommelmdalisay@gmail.com>
2025-08-06 18:52:23 +0800

    list

---
+++ list.py
@@ -0,0 +1,7 @@
+even_numbers = [2, 4, 6, 8, 10]
+bar = 90
+names = ['Molly', 'Steven', 'Will', 'Alicia', 'Adriana']
+info = ['Alicia', 27, 1550.87, True, bar]
+print(even_numbers)
+print(names)
+print(info)
