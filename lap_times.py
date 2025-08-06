43df9a5bf022ece17e1598f44b3fedf7f1ba20ad
rommel dalisay <rommelmdalisay@gmail.com>
2025-08-06 13:55:32 +0800

    first commit

--- lap_times.py
+++ lap_times.py
@@ -4,16 +4,24 @@ def main():
 
 def lap_time(num_laps):
     ave_laps = 0
+    largest = 0
     for i in range(num_laps):
-        #largest
-
-
         time = float(input("enter your running time"))
+        #largest input is 10
+
+        #loop2 input is 4
+        #loop3 input is 90
+        if time > largest:
+            largest = time
+            #largest = 10
+
+
         ave_laps += time
 
     
     average = ave_laps / num_laps
     print("average lap times", average )
+    print("the slowest time is: ", largest)
 
 
 main()
