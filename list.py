fc1e9b4899a12afd8e96721cffc73b0d73658c69
rommel dalisay <rommelmdalisay@gmail.com>
2025-08-06 19:13:25 +0800

    search list

--- list.py
+++ list.py
@@ -1,7 +1,56 @@
-even_numbers = [2, 4, 6, 8, 10]
-bar = 90
-names = ['Molly', 'Steven', 'Will', 'Alicia', 'Adriana']
-info = ['Alicia', 27, 1550.87, True, bar]
-print(even_numbers)
-print(names)
-print(info)
+# even_numbers = [2, 4, 6, 8, 10]
+# bar = 90
+# names = ['Molly', 'Steven', 'Will', 'Alicia', 'Adriana']
+# info = ['Alicia', 27, 1550.87, True, bar]
+# print(even_numbers)
+# print(names)
+# print(info)
+
+numbers = [5, 10, 15, 20]
+print(numbers)
+list1 = list()
+list2 = []
+print(list2)
+numbers = list(range(5))
+print(numbers)
+
+numbers = [1] * 5
+print(numbers)
+
+numbers = [99, 100, 101, 102]
+print(len(numbers))
+# for n in numbers:
+#     print(n)
+
+
+# numbers = [1, 2, 3, 4, 5]
+# print(numbers)
+# numbers[0] = 99
+# print(numbers)
+
+girl_names = ['Joanne', 'Karen', 'Lori']
+boy_names = ['Chris', 'Jerry', 'Will']
+all_names = girl_names + boy_names
+print(all_names)
+
+days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
+'Thursday', 'Friday', 'Saturday']
+
+mid_days = days[2:]
+print(mid_days)
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+print(numbers[:1])
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numbers)
+
+print(numbers[1:8:2])
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numbers)
+
+print(numbers[-5:])
+
