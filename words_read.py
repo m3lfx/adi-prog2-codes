5086b899aa0f9c7ffd19f8596dc5bc197b51c2d6
rommel dalisay <rommelmdalisay@gmail.com>
2025-08-09 13:47:16 +0800

    word list

--- words_read.py
+++ words_read.py
@@ -1,17 +1,16 @@
 def main():
-    word_file = open('words.txt', 'r')
-    word_list = []
-    line = word_file.readline()
 
     num_words = 0
     ave_wordlength = 0
     total_length = 0
     long_word = ""
-    temp_word = ""
+    word_list = []
     len_word = []
 
+    word_file = open('words.txt', 'r')
+    line = word_file.readline()
+
     while line != '':
-        temp_word = line
         num_words += 1
         print(line)
         word_list.append(line.rstrip('\n'))
@@ -26,6 +25,5 @@ def main():
     ave_wordlength = total_length / num_words
     print(f"total number of words in the file {num_words}")
     print(f"average length of words in file {ave_wordlength}")
-    
     print(f"the longest word is {long_word} ")
 main()
