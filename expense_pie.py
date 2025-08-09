ff36a612f13d16042f12c1a4b8377fe928576219
rommel dalisay <rommelmdalisay@gmail.com>
2025-08-09 14:51:32 +0800

    expenses

--- expense_pie.py
+++ expense_pie.py
@@ -1,9 +1,14 @@
+import matplotlib.pyplot as plt
+
 def main():
     print()
     write_expense_file()
+    expenses = read_expense_file()
+    create_pie(expenses)
+
 
 def write_expense_file():
-    expense_file = open('expense.txt', 'a')
+    expense_file = open('expense.txt', 'w')
     
     print('Enter data for expense #', sep='')
     rent = float(input('Rent Amount: '))
@@ -23,4 +28,35 @@ def write_expense_file():
 
     # Display a blank line.
     print()
+    expense_file.close()
+
+def read_expense_file():
+    expense_list = []
+    expense_file = open('expense.txt', 'r')
+    expense_list = expense_file.readlines()
+    print(expense_list)
+
+    index = 0
+    while index < len(expense_list):
+        expense_list[index] = expense_list[index].rstrip('\n')
+        expense_list[index] = float(expense_list[index])
+        
+        index +=1
+    print(expense_list)
+    return expense_list
+
+def create_pie(expenses):
+    
+    slice_labels = ['Rent', 'Gas', 'food', 'clothes', 'car payment', 'misc']
+
+    plt.pie(expenses, labels=slice_labels)
+
+    # Add a title.
+    plt.title('Gastos ko')
+
+    # Display the pie chart.
+    plt.show()
+
+
+
 main()
