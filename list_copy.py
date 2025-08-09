cdf00aa326290dc07c698483a7c40f67ede60848
rommel dalisay <rommelmdalisay@gmail.com>
2025-08-09 08:51:54 +0800

    list

---
+++ list_copy.py
@@ -0,0 +1,16 @@
+list1 = [1, 2, 3, 4]
+# Assign the list to the list2 variable.
+list2 = list1
+
+print(list1)
+
+print(list2)
+
+list1 = [1, 2, 3, 4]
+# Create an empty list.
+list2 = []
+
+for item in list1:
+    list2.append(item)
+
+print(list2)
---
+++ list_total.py
@@ -0,0 +1,14 @@
+def main():
+    # Create a list.
+    numbers = [2, 4, 6, 8, 10]
+    # Create a variable to use as an accumulator.
+    total = 0
+
+    # for value in numbers:
+    #     total += value
+
+    # Display the total of the list elements.
+    print('The total of the elements is', sum(numbers))
+
+# Call the main function.
+main()
