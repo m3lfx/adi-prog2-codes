b1853aecdea8917ceb3bb618b2361c1de268ee26
rommel dalisay <rommelmdalisay@gmail.com>
2025-08-09 10:54:11 +0800

    line

---
+++ list_tuples.py
@@ -0,0 +1,15 @@
+my_tuple = [1, 2, 3, 4, 5]
+print(my_tuple)
+
+names = ('Holly', 'Warren', 'Ashley')
+# names[1] = "bar"
+for i in range(len(names)):
+    print(names[i])
+
+number_tuple = (1, 2, 3)
+print(number_tuple)
+number_list = list(number_tuple)
+print(number_list)
+str_list = ['one', 'two', 'three']
+str_tuple = tuple(str_list)
+print(str_tuple)
