1167b64a1e7ceccb7eb0a7413e2e9648f9390f7f
rommel dalisay <rommelmdalisay@gmail.com>
2025-08-13 17:49:13 +0800

    oop

---
+++ coins.py
@@ -0,0 +1,29 @@
+import coin
+
+def main():
+    # Create three objects from the Coin class.
+    coin1 = coin.Coin()
+    coin2 = coin.Coin()
+    coin3 = coin.Coin()
+
+    print('I have three coins with these sides up:')
+    print(coin1.get_sideup())
+    print(coin2.get_sideup())
+    print(coin3.get_sideup())
+    print()
+
+    print('I am tossing all three coins ...')
+    print()
+    coin1.toss()
+    coin2.toss()
+    coin3.toss()
+
+    # Display the side of each coin that is facing up.
+    print('Now here are the sides that are up:')
+    print(coin1.get_sideup())
+    print(coin2.get_sideup())
+    print(coin3.get_sideup())
+    print()
+
+# Call the main function.
+main()
