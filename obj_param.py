feaae10812ce1a964977052189ca338098e5e91b
rommel dalisay <rommelmdalisay@gmail.com>
2025-08-13 17:56:55 +0800

    pickle objects

---
+++ obj_param.py
@@ -0,0 +1,23 @@
+import coin
+
+# main function
+def main():
+    # Create a Coin object.
+    my_coin = coin.Coin()
+
+    # This will display 'Heads'.
+    print(my_coin.get_sideup())
+
+    # Pass the object to the flip function.
+    flip(my_coin)
+
+    # This might display 'Heads', or it might
+    # display 'Tails'.
+    print(my_coin.get_sideup())
+
+# The flip function flips a coin.
+def flip(coin_obj):
+    coin_obj.toss()
+
+# Call the main function.
+main()
