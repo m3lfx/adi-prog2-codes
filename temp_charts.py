5c13602125f64308aa6390fa5f7cf6a867ac0293
rommel dalisay <rommelmdalisay@gmail.com>
2025-08-13 13:15:13 +0800

    csv chart

---
+++ temp_charts.py
@@ -0,0 +1,23 @@
+#create a program that will let the user enter a number of years and the monthly average temperature in celsius for 
+# each year. the temperature data will be written to a file in a csv format. read the contents of
+# the csv file and generate a line chart with the average of temperature for each year
+
+import csv
+import matplotlib.pyplot as plt
+
+MONTHS = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+]
+def main():
+    start = int(input("Enter starting year   : "))
+    years = int(input(f"Enter the number of years starting from {start}  : "))
+
+def write_temp_csv(filename, start, years):
+
+def read_temp_csv(filename):
+
+def create_graph():
+
+
+main():
